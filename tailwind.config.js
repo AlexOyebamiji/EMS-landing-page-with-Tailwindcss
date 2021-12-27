2f51e451a3651e6d2d2067e504814c0b40e5edf2
Oyebamiji Alex <alexferanmi390@gmail.com>
2021-12-27 06:44:21 +0100

    learn tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'lightorange': '#ffcd9e',
+      'sectionorange': '#ffe9d6',
+      'cartorange': '#ff7a00',
+      'shoporange': '#ff9a3d',
+      'white': '#ffffff',
+      'black': '#000000'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
